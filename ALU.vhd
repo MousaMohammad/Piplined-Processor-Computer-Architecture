0a6e6c6ab2e918fd57713d6d89fc7ff51cffca46
Mousa Mohammad Abdulmaxod <mousamax101@gmail.com>
2022-05-24 22:54:27 +0200

    Update ALU.vhd

--- ALU.vhd
+++ ALU.vhd
@@ -16,15 +16,32 @@ ARCHITECTURE archB OF ALU IS
 -- INC signals --
 signal incRes: std_logic_vector(31 DOWNTO 0);
 signal incCout: std_logic;
+-- Add signals --
+signal addRes: std_logic_vector(31 DOWNTO 0);
+signal addCout: std_logic;
+-- Sub signals --
+signal subRes: std_logic_vector(31 DOWNTO 0);
+signal subCout: std_logic;
+
 BEGIN
     -- NOT --
     F <= NOT A WHEN opCode = "000"
     -- INC --
-    ELSE incRes WHEN opCode = "001";
+    ELSE incRes WHEN opCode = "001"
     -- ADD --
+    ElSE addRes WHEN opCode = "010"
+    -- SUB --
+    ELSE subRes WHEN opCode = "011"
+    -- AND --
+    ELSE A AND B WHEN opCode = "100";
     
 
-    INC :entity work.my_nadder GENERIC map(32) port map(A, "00000000000000000000000000000001",CCR(2), incRes, incCout);
-Cout <= incCout WHEN opCode = "001";
- 
+    INC :entity work.my_nadder GENERIC map(32) port map(A, "00000000000000000000000000000001",'0', incRes, incCout);
+    ADD :entity work.my_nadder GENERIC map(32) port map(A, B, '0', addRes, addCout);
+    SUB :entity work.my_nadder GENERIC map(32) port map(A, NOT B, '1', subRes, subCout);
+    -- carry out --
+Cout <= incCout WHEN opCode = "001"
+        ELSE addCout WHEN opCode = "010"
+        ELSE subCout WHEN opCode = "011";
+
 END ARCHITECTURE;
