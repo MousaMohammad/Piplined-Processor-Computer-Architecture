54079e0874968a13cbcc90ee716d495bd917f15e
Mousa Mohammad Abdulmaxod <mousamax101@gmail.com>
2022-05-24 14:24:17 +0200

    tested flagControl

--- flagControl.vhd
+++ flagControl.vhd
@@ -16,6 +16,7 @@ begin
     -- CCR(0) = Z flag, CCR(1) = N flag, CCR(2) = C flag --
     CCR(0) <= '1' WHEN aluRes = "00000000000000000000000000000000" ELSE '0';
     CCR(1) <= '1' WHEN aluRes < "00000000000000000000000000000000" ELSE '0';
-    CCR(2) <= carry WHEN setc = '0' ELSE '1';
+    CCR(2) <= carry WHEN setc = '0' and (carry = '1' or carry =  '0')
+    ELSE '1' WHEN setc = '1';
 
 end flagarch ; -- flagarch
