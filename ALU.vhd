3e652cd31a71db4c170109d3f91ffaf1fc1c5352
Mousa Mohammad Abdulmaxod <mousamax101@gmail.com>
2022-05-24 13:03:55 +0200

    added nbitAdder

--- ALU.vhd
+++ ALU.vhd
@@ -13,43 +13,13 @@ GENERIC (n : integer := 32);
 END Entity;
 
 ARCHITECTURE archB OF ALU IS
+signal incRes: std_logic_vector(31 DOWNTO 0);
 BEGIN
 
     -- NOT --
     F <= NOT A WHEN opCode = "000"
-    ELSE F;
-    
-    process( clk ) is
-    begin
-            -- SETC --
-        if opCode = "000" then 
-            CCR(2) <= '1';
-            -- Not Rdst --
-        elsif opCode = "001" then
-            F <= NOT A;
-            -- Z flag --
-            if NOT A = (others => '0') then
-                CCR(0) <= '1';
-            else
-                CCR(0) <= '0';
-            end if;
-            -- N flag --
-            if NOT A < (others => '0') then
-                CCR(1) <= '1';
-            else
-                CCR(1) <= '0';
-            end if;
-            -- incerement A --
-        elsif opCose = "010" then
-            F <= A + '1';
-            
-            
-            
-
-        end if;
-            
-        
-    end process ;
+    -- INC --
+    ELSE incRes WHEN opCode = "001";
 
 Cout <= '0';
  
