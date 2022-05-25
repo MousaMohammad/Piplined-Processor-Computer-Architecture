766dbd3ed8bec09c630bd7d80b1d0847947a5a53
Nader-Youhanna <naderyouhanna@gmail.com>
2022-05-25 23:15:44 +0200

    Added Generic Tri State Buffer

--- Reg.vhd
+++ Reg.vhd
@@ -2,12 +2,11 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 
 ENTITY Reg IS
-generic (n : integer := 32);
+    GENERIC (n : INTEGER := 32);
     PORT (
         Rst, Clk, Enable : IN STD_LOGIC;
-
-        RegInput : IN STD_LOGIC_VECTOR(n-1 DOWNTO 0);
-        RegOutput : OUT STD_LOGIC_VECTOR(n-1 DOWNTO 0));
+        RegInput : IN STD_LOGIC_VECTOR(n - 1 DOWNTO 0);
+        RegOutput : OUT STD_LOGIC_VECTOR(n - 1 DOWNTO 0));
 
 END ENTITY;
 
