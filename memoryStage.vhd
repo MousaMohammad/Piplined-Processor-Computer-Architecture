2c9c9fe8ddd5d7d41f6ff65c0d5adfc02a09d415
Nader-Youhanna <naderyouhanna@gmail.com>
2022-05-27 18:05:06 +0200

    Update memoryStage.vhd

--- memoryStage.vhd
+++ memoryStage.vhd
@@ -8,7 +8,11 @@ ENTITY memoryStage IS
         Rst : IN STD_LOGIC;
         CCR : IN STD_LOGIC_VECTOR(2 downto 0);
         jumpControlSignal: IN STD_LOGIC_VECTOR(2 DOWNTO 0);
-        memWriteRead: IN STD_LOGIC;
-        SPControlSignal: IN STD_LOGIC_VECTOR(2 downto 0)
+        memWriteReadControlSignal: IN STD_LOGIC;        -- 0 -> Write / 1 -> Read
+        SPControlSignal: IN STD_LOGIC_VECTOR(2 downto 0)    -- 00 -> No Change / 01 -> +1 / 10 -> -1 
     );
 END ENTITY;
+
+
+ARCHITECTURE memArch OF memoryStage IS
+BEGIN
