5de160d15674dd4370935116169f6bd2d6242e3a
Nader-Youhanna <naderyouhanna@gmail.com>
2022-05-27 18:02:39 +0200

    Finished memory / WB buffer

--- MEMWB_buf.vhd
+++ MEMWB_buf.vhd
@@ -22,7 +22,16 @@ BEGIN
     PROCESS (Rst, Clk) IS
     BEGIN
         IF Rst = '1' THEN
+            writeBackSignal_Out <= (others => '0');
+            Memory_Output_Out <= (others => '0');
+            ALU_Output_Out <= (others => '0');
+            writeAddressRegFile_Out <= (others => '0');
         ELSIF rising_edge(Clk) THEN
+            writeBackSignal_Out <= writeBackSignal_In;
+            Memory_Output_Out <= Memory_Output_In;
+            ALU_Output_Out <= ALU_Output_In;
+            writeAddressRegFile_Out <= writeAddressRegFile_In;
+        END IF;
         END IF;
     END PROCESS;
 END archMemWB_buf;
