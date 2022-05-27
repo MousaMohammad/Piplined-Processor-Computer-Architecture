070665fb5934c0dba933d5f74c03f9faa3713143
Mousa Mohammad Abdulmaxod <mousamax101@gmail.com>
2022-05-27 15:19:46 +0200

    working on buffers

--- EX_MEM_buf.vhd
+++ EX_MEM_buf.vhd
@@ -5,11 +5,27 @@ entity ExMem_buf is
   port (
     Rst, Clk : IN STD_LOGIC;
     -- Ex ports --
-    
+    PC_i : IN STD_LOGIC_VECTOR(31 downto 0);
+    CCR_i : IN STD_LOGIC_VECTOR(2 downto 0);
+    Alu_i : IN STD_LOGIC_VECTOR(31 downto 0);
+    PC_o : OUT STD_LOGIC_VECTOR(31 downto 0);
+    CCR_o : OUT STD_LOGIC_VECTOR(2 downto 0);
+    Alu_o : OUT STD_LOGIC_VECTOR(31 downto 0)
   );
 end entity;
 
 architecture arch of ExMem_buf is
 begin
-
+    process(Rst, Clk) is
+        begin
+            if Rst = '1' then
+                PC_o <= (others => '0');
+                CCR_o <= (others => '0');
+                Alu_o <= (others => '0');
+            elsif rising_edge(Clk) then
+                PC_o <= PC_i;
+                CCR_o <= CCR_i;
+                Alu_o <= Alu_i;
+            end if;
+        end process;
 end arch ; -- arch
--- EX_bufs_integ.vhd
+++ EX_bufs_integ.vhd
@@ -1,1 +1,37 @@
+Library ieee;
+Use ieee.std_logic_1164.all;
 
+entity exBufsInteg is
+  port (
+    Rst, Clk : IN STD_LOGIC;
+    ExeSrc, SETC : IN STD_LOGIC;
+    AluOpCode :IN std_logic_vector(2 downto 0);
+    Rsrc1, Rsrc2, Immediate : IN std_logic_vector(31 downto 0);
+    PC : IN std_logic_vector(31 downto 0);
+    --- Outputs ---
+    PC_o : OUT STD_LOGIC_VECTOR(31 downto 0);
+    CCR_o : OUT STD_LOGIC_VECTOR(2 downto 0);
+    Alu_o : OUT STD_LOGIC_VECTOR(31 downto 0)
+  ) ;
+end entity;
+
+architecture arch of exBufsInteg is
+--- signals from IDEx buf to execute stage ---
+signal ExeSrc_o : std_logic;
+signal SETC_o : std_logic;
+signal AluOpCode_o : std_logic_vector(2 downto 0);
+signal Rsrc1_o : std_logic_vector(31 downto 0);
+signal Rsrc2_o : std_logic_vector(31 downto 0);
+signal Immediate_o : std_logic_vector(31 downto 0);
+signal PC_IDbuf : std_logic_vector(31 downto 0);
+--- signals from execute stage to EX Mem buffer ---
+signal CCR_Ex : std_logic_vector(2 downto 0);
+signal Alu_Ex : std_logic_vector(31 downto 0);
+signal PC_Ex : std_logic_vector(31 downto 0);
+
+begin
+
+    -- IDEx buf to execute stage --
+    ID_Ex_buf
+
+end architecture ; -- arch
