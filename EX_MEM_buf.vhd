b63290df016b153198b37c79094639f67e79547b
Mousa Mohammad Abdulmaxod <mousamax101@gmail.com>
2022-05-27 14:36:36 +0200

    added buffers

---
+++ EX_MEM_buf.vhd
@@ -0,0 +1,15 @@
+Library ieee;
+Use ieee.std_logic_1164.all;
+
+entity ExMem_buf is
+  port (
+    Rst, Clk : IN STD_LOGIC;
+    -- Ex ports --
+    
+  );
+end entity;
+
+architecture arch of ExMem_buf is
+begin
+
+end arch ; -- arch
