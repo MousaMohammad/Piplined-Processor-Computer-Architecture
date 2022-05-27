cd977762cf49b33c562ff22e1b15fa2cb3379621
Nader-Youhanna <naderyouhanna@gmail.com>
2022-05-27 20:23:02 +0200

    Memory Stage

---
+++ MEMWB_buf.vhd
@@ -0,0 +1,34 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+
+ENTITY MemWB_buf IS
+    PORT (
+        Rst, Clk : IN STD_LOGIC;
+        writeBackSignal_In : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
+        writeBackSignal_Out : OUT STD_LOGIC_VECTOR(1 DOWNTO 0);
+        Memory_Output_In : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+        Memory_Output_Out : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+        ALU_Output_In : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+        ALU_Output_Out : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+        writeAddressRegFile_In : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+        writeAddressRegFile_Out : OUT STD_LOGIC_VECTOR(2 DOWNTO 0)
+    );
+END ENTITY;
+
+ARCHITECTURE archMemWB_buf OF MemWB_buf IS
+BEGIN
+    PROCESS (Rst, Clk) IS
+    BEGIN
+        IF Rst = '1' THEN 
+            writeBackSignal_Out <= (OTHERS => '0');
+            Memory_Output_Out <= (OTHERS => '0');
+            ALU_Output_Out <= (OTHERS => '0');
+            writeAddressRegFile_Out <= (OTHERS => '0');
+        ELSIF rising_edge(Clk) THEN
+            writeBackSignal_Out <= writeBackSignal_In;
+            Memory_Output_Out <= Memory_Output_In;
+            ALU_Output_Out <= ALU_Output_In;
+            writeAddressRegFile_Out <= writeAddressRegFile_In;
+        END IF;
+    END PROCESS;
+END archMemWB_buf;
--- memoryStage.vhd
+++ memoryStage.vhd
@@ -4,12 +4,33 @@ USE ieee.std_logic_1164.ALL;
 ENTITY memoryStage IS
 
     PORT (
-        Clk : IN std_logic;
-        Rst : IN std_logic;
-        mem_read : IN std_logic;
-        mem_write : IN std_logic;
-        mem_address : IN std_logic_vector(31 downto 0);
-        mem_data_in : IN std_logic_vector(31 downto 0);
-        mem_data_out : OUT std_logic_vector(31 downto 0)
+        Clk : IN STD_LOGIC;
+        Rst : IN STD_LOGIC;
+        CCR : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+        jumpControlSignal : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+        memWriteReadControlSignal : IN STD_LOGIC; -- 0 -> Write / 1 -> Read
+        SPControlSignal : IN STD_LOGIC_VECTOR(2 DOWNTO 0); -- 00 -> No Change / 01 -> +1 / 10 -> -1 
+        ALU_Output : IN STD_LOGIC_VECTOR(32 DOWNTO 0);
+        writeData : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+        Memory_Output : IN STD_LOGIC_VECTOR(32 DOWNTO 0);
+        readData : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+        SP : OUT STD_LOGIC_VECTOR(19 DOWNTO 0);
+        PC_Mux_Selector: OUT STD_LOGIC
     );
 END ENTITY;
+ARCHITECTURE memArch OF memoryStage IS
+BEGIN
+
+    PROCESS (Rst, Clk)
+    BEGIN
+        IF Rst = '1' THEN
+            SP <= x"FFFFF";
+            readData <= (OTHERS => '0');
+            PC_Mux_Selector <= '0';
+        ELSIF rising_edge(Clk) THEN
+
+        END IF;
+
+    END PROCESS;
+
+END memArch;
