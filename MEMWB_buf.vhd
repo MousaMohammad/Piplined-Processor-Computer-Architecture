f94b791e9dfb42fa612ca2c545cead35647c63a7
Nader-Youhanna <naderyouhanna@gmail.com>
2022-05-28 01:34:30 +0200

    Memory Stage + MEM/WB Buffer

--- MEMWB_buf.vhd
+++ MEMWB_buf.vhd
@@ -4,8 +4,8 @@ USE ieee.std_logic_1164.ALL;
 ENTITY MemWB_buf IS
     PORT (
         Rst, Clk, Enable : IN STD_LOGIC;
-        writeBackSignal_In : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
-        writeBackSignal_Out : OUT STD_LOGIC_VECTOR(1 DOWNTO 0);
+        writeBackControlSignal_In : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
+        writeBackControlSignal_Out : OUT STD_LOGIC_VECTOR(1 DOWNTO 0);
         Memory_Output_In : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
         Memory_Output_Out : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
         ALU_Output_In : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
@@ -20,12 +20,12 @@ BEGIN
     PROCESS (Rst, Clk) IS
     BEGIN
         IF Rst = '1' THEN 
-            writeBackSignal_Out <= (OTHERS => '0');
+            writeBackControlSignal_Out <= (OTHERS => '0');
             Memory_Output_Out <= (OTHERS => '0');
             ALU_Output_Out <= (OTHERS => '0');
             writeAddressRegFile_Out <= (OTHERS => '0');
         ELSIF rising_edge(Clk) AND Enable = '1' THEN
-            writeBackSignal_Out <= writeBackSignal_In;
+            writeBackControlSignal_Out <= writeBackControlSignal_In;
             Memory_Output_Out <= Memory_Output_In;
             ALU_Output_Out <= ALU_Output_In;
             writeAddressRegFile_Out <= writeAddressRegFile_In;
--- memoryStage.vhd
+++ memoryStage.vhd
@@ -3,32 +3,63 @@ USE ieee.std_logic_1164.ALL;
 USE IEEE.numeric_std.ALL;
 
 ENTITY memoryStage IS
-
     PORT (
         Clk : IN STD_LOGIC;
         Rst : IN STD_LOGIC;
-        CCR : IN STD_LOGIC_VECTOR(2 DOWNTO 0); --ZNC
-        jumpControlSignal : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
-        memWriteReadControlSignal : IN STD_LOGIC; -- 0 -> Write / 1 -> Read
-        SPControlSignal : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
-        ALU_Output : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
-        writeData : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
-        Memory_Output : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
-        readData : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
-        SP : OUT STD_LOGIC_VECTOR(19 DOWNTO 0);
-        PC_Mux_Selector : OUT STD_LOGIC;
-        address_In : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
-        address_Out : OUT STD_LOGIC_VECTOR(19 DOWNTO 0)
+        CCR : IN STD_LOGIC_VECTOR(2 DOWNTO 0);                              --ZNC   -   Comes from EX/MEM buff
+        jumpControlSignal : IN STD_LOGIC_VECTOR(2 DOWNTO 0);                -- Comes from EX/MEM buff
+        memWriteControlSignal_In : IN STD_LOGIC;                            -- Comes from EX/MEM buff
+        memReadControlSignal_In : IN STD_LOGIC;                             -- Comes from EX/MEM buff
+        SPControlSignal : IN STD_LOGIC_VECTOR(3 DOWNTO 0);                  -- Comes from EX/MEM buff
+        ALU_Output_In : IN STD_LOGIC_VECTOR(31 DOWNTO 0);                   -- Comes from EX/MEM buff
+        writeBackControlSignal_In: IN STD_LOGIC_VECTOR(1 DOWNTO 0);         -- Comes from EX/MEM buff
+        writeDataBuff_In: IN STD_LOGIC_VECTOR(31 DOWNTO 0);                 -- Comes from EX/MEM buff
+        PC : IN STD_LOGIC_VECTOR(31 DOWNTO 0);                              -- Comes from EX/MEM buff
+        RegFileAddressWB_In: IN STD_LOGIC_VECTOR(2 DOWNTO 0);               -- Comes from EX/MEM buff
+        Memory_Output : IN STD_LOGIC_VECTOR(31 DOWNTO 0);                   -- Comes from memory
+        memWriteControlSignal_Mem : OUT STD_LOGIC;                          -- To be sent to memory
+        memReadControlSignal_Mem : OUT STD_LOGIC;                           -- To be sent to memory
+        writeData_Mem : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);                  -- To be sent to memory
+        address_Out : OUT STD_LOGIC_VECTOR(19 DOWNTO 0);                    -- To be sent to memory
+        writeBackControlSignal_Out: OUT STD_LOGIC_VECTOR(1 DOWNTO 0);       -- To be sent to MEM/WB buff
+        ALU_Output_Out : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);                 -- To be sent to MEM/WB buff
+        readData_Mem : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);                   -- To be sent to MEM/WB buff
+        RegFileAddressWB_Out: OUT STD_LOGIC_VECTOR(2 DOWNTO 0);             -- To be sent to MEM/WB buff
+        PC_Mux_Selector : OUT STD_LOGIC                                     -- To be sent to Fetch
+
     );
 END ENTITY;
+
 ARCHITECTURE memArch OF memoryStage IS
+SIGNAL SP_After, SP_Before: STD_LOGIC_VECTOR(31 DOWNTO 0);
+
 BEGIN
+
+
+    memWriteControlSignal_Mem <= memWriteControlSignal_In;
+    memReadControlSignal_Mem <= memReadControlSignal_In;
+
+    writeData_Mem <= ALU_Output_In WHEN SPControlSignal = "0110"             -- PUSH Only
+    ELSE PC WHEN SPControlSignal = "0111"                                    -- CALL Only
+    ELSE writeDataBuff_In WHEN SPControlSignal(3) = '0' AND memWriteControlSignal_In = '1'
+    ELSE (Others => '0');
+
+    address_Out <= ALU_Output_In(19 DOWNTO 0) WHEN SPControlSignal(3) = '0'     -- LOAD and STORE
+    ELSE SP_After(19 DOWNTO 0) WHEN SPControlSignal = "1000" OR SPControlSignal = "1001" OR SPControlSignal = "1101" --POP/RET/RTI
+    ELSE SP_Before(19 DOWNTO 0) WHEN SPControlSignal = "1010" OR SPControlSignal = "1011" OR SPControlSignal = "1100"; --PUSH/CALL/INT/ 
+
+    writeBackControlSignal_Out <= writeBackControlSignal_In;
+
+    ALU_Output_Out <= ALU_Output_In;
+
+    readData_Mem <= Memory_Output;
+
+    RegFileAddressWB_Out <= RegFileAddressWB_In;
 
     PROCESS (Rst)
     BEGIN
         IF Rst = '1' THEN
-            SP <= x"FFFFF";
-            readData <= (OTHERS => '0');
+            readData_Mem <= (OTHERS => '0');
             PC_Mux_Selector <= '0';
         END IF;
     END PROCESS;
@@ -49,8 +80,16 @@ BEGIN
         ELSE
         '0' WHEN jumpControlSignal = "111" AND CCR(0) = '0';
 
-    address_Out <= address_In(19 DOWNTO 0) WHEN SPControlSignal(2) = '0';
-    -- ELSE  WHEN SPControlSignal(2) = '1' AND SPControlSignal(1) = '0';
+
+   
+    spReg: ENTITY work.sp_module PORT MAP(Clk => Clk, Rst => Rst, Enable => SPControlSignal(3),
+                                            SP_In => SP_After(19 DOWNTO 0), SP_Out => SP_Before(19 DOWNTO 0));
+    SP_After <= SP_Before WHEN SPControlSignal(3) = '0'
+    ELSE STD_LOGIC_VECTOR(UNSIGNED(SP_Before) - 1) WHEN SPControlSignal = "0110" OR SPControlSignal = "0111"
+    ELSE STD_LOGIC_VECTOR(UNSIGNED(SP_Before) + 1) WHEN SPControlSignal = "0100" OR SPControlSignal = "0101";
+
+
+    
    
 
 END memArch;
