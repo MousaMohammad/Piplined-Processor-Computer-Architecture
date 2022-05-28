7d82cd18d904f3690951b8fc6a68e4b6b318618f
Nader-Youhanna <naderyouhanna@gmail.com>
2022-05-28 12:12:04 +0200

    Update EX_MEM_buf.vhd

--- EX_MEM_buf.vhd
+++ EX_MEM_buf.vhd
@@ -1,35 +1,59 @@
-Library ieee;
-Use ieee.std_logic_1164.all;
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
 
-entity ExMem_buf is
-  port (
+ENTITY ExMem_buf IS
+  PORT (
     Rst, Clk : IN STD_LOGIC;
     -- Ex ports --
-    CCR_i : IN STD_LOGIC_VECTOR(2 downto 0);
-    PC_i : IN STD_LOGIC_VECTOR(31 downto 0);
-    Alu_i : IN STD_LOGIC_VECTOR(31 downto 0);
-    WriteData_i : IN STD_LOGIC_VECTOR(31 downto 0);
-    PC_o : OUT STD_LOGIC_VECTOR(31 downto 0);
-    CCR_o : OUT STD_LOGIC_VECTOR(2 downto 0);
-    Alu_o : OUT STD_LOGIC_VECTOR(31 downto 0);
-    WriteData_o : OUT STD_LOGIC_VECTOR(31 downto 0)
+    CCR_i : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+    PC_i : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+    Alu_i : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+    WriteData_i : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+    PC_o : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+    CCR_o : OUT STD_LOGIC_VECTOR(2 DOWNTO 0);
+    Alu_o : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+    WriteData_o : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+    jumpControlSignal_i : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+    jumpControlSignal_o : OUT STD_LOGIC_VECTOR(2 DOWNTO 0);
+    memWriteControlSignal_i : IN STD_LOGIC;
+    memWriteControlSignal_o : OUT STD_LOGIC;
+    memReadControlSignal_i : IN STD_LOGIC;
+    memReadControlSignal_o : OUT STD_LOGIC;
+    SPControlSignal_i : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+    SPControlSignal_o : OUT STD_LOGIC_VECTOR(3 DOWNTO 0);
+    writeBackControlSignal_i : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
+    writeBackControlSignal_o : OUT STD_LOGIC_VECTOR(1 DOWNTO 0);
+    RegFileAddressWB_i : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+    RegFileAddressWB_o : OUT STD_LOGIC_VECTOR(2 DOWNTO 0)
   );
-end entity;
+END ENTITY;
 
-architecture arch of ExMem_buf is
-begin
-    process(Rst, Clk) is
-        begin
-            if Rst = '1' then
-                PC_o <= (others => '0');
-                CCR_o <= (others => '0');
-                Alu_o <= (others => '0');
-                WriteData_o <= (others => '0');
-            elsif rising_edge(Clk) then
-                PC_o <= PC_i;
-                CCR_o <= CCR_i;
-                Alu_o <= Alu_i;
-                WriteData_o <= WriteData_i;
-            end if;
-        end process;
-end arch ; -- arch
+ARCHITECTURE arch OF ExMem_buf IS
+BEGIN
+  PROCESS (Rst, Clk) IS
+  BEGIN
+    IF Rst = '1' THEN
+      PC_o <= (OTHERS => '0');
+      CCR_o <= (OTHERS => '0');
+      Alu_o <= (OTHERS => '0');
+      WriteData_o <= (OTHERS => '0');
+      jumpControlSignal_o <= (OTHERS => '0');
+      memWriteControlSignal_o <= '0';
+      memReadControlSignal_o <= '0';
+      SPControlSignal_o <= (OTHERS => '0');
+      writeBackControlSignal_o <= (OTHERS => '0');
+      RegFileAddressWB_o <= (OTHERS => '0');
+    ELSIF rising_edge(Clk) THEN
+      PC_o <= PC_i;
+      CCR_o <= CCR_i;
+      Alu_o <= Alu_i;
+      WriteData_o <= WriteData_i;
+      jumpControlSignal_o <= jumpControlSignal_i;
+      memWriteControlSignal_o <= memWriteControlSignal_i;
+      memReadControlSignal_o <= memReadControlSignal_i;
+      SPControlSignal_o <= SPControlSignal_i;
+      writeBackControlSignal_o <= writeBackControlSignal_i;
+      RegFileAddressWB_o <= RegFileAddressWB_i;
+    END IF;
+  END PROCESS;
+END arch; -- arch
